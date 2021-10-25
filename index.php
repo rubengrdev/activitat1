4188131170044d001b7f75e83adafbff44909581
rubengorodri <38557563+rubengorodri@users.noreply.github.com>
2021-10-25 22:04:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,51 +1,16 @@
 <?php
 session_start();
- //establecer errores
- ini_set('display_errors', 'On');
+//ini_set('display_errors', 'On');
+//require connection variables 
 require 'config.php';
+//require "router"
 require APP.'/src/route.php';
 
-
-
-
+//getRoute gets $_REQUEST information from url
 $controller=getRoute();
 require APP.'/src/controllers/'.$controller.'.php';
 
 
 
-  /*
-  try{
-       $stmt=$gdb->prepare("select * from users");
-       $stmt->execute();
-       /**while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-          print($row['username']."<br>");
-       }
-       /**
-        * 
-        **
-
-        $rows = $stmt->fetchAll();
-        foreach($rows as $row){
-            //secho render($row);
-             echo $row['username']."<br>";
-             
-        }
-        /**
-    * $stmt = $gdb->prepare("INSERT INTO users(username,passwd) values (?,?)");
-    * $stmt->bindParam(1,$usr);
-    * $stmt->bindParam(2,$pass);
-   *  //ejecución
-  *   $stmt->execute();
-      **
-  }catch(PDOException $e){
-      echo $e->getMessage();
-  }
-  */
-  //$route=getRoute();
-  //var_dump($route);
-  //echo render($route,["nom"=>"SCHOOL"]);
-  
-     //$gdb->query();
-     //$gdb->exec();
 
 
